addf1723f5ec47b2c50e578f92da432945d2310d
Alex Veltman <alexveltman@protonmail.com>
2020-12-02 12:29:29 +0200

    Added text storage into sqlite database

--- migrations/2020-12-01-175202_create_posts/up.sql
+++ migrations/2020-12-01-175202_create_posts/up.sql
@@ -1,7 +1,30 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE posts (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     date DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
     subtitle VARCHAR(1024) NOT NULL,
     content TEXT NOT NULL
-)
+);
+
+CREATE TABLE pages (
+    page_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(32) NOT NULL
+);
+
+INSERT INTO pages (name) VALUES ("Home");
+INSERT INTO pages (name) VALUES ("Blog");
+INSERT INTO pages (name) VALUES ("Resume");
+
+CREATE TABLE texts (
+    text_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    page INTEGER NOT NULL,
+    description VARCHAR(64) NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY(page) REFERENCES pages(page_id)
+);
+
+INSERT INTO texts (page, description, content)
+VALUES (1,"about me", "My name is Alex Veltman and I am an undergraduate student at the University of Cape Town. My interests lie in Physics, Mathematics and Computer Science. This site was written in Python using <a href='https://flask.palletsprojects.com/'>Flask</a> and the source code for this site can be found <a href='https://github.com/codecalec/PersonalWebsiteFlask'>here</a>.");
+
--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,4 +1,11 @@
 table! {
+    pages (page_id) {
+        page_id -> Integer,
+        name -> Text,
+    }
+}
+
+table! {
     posts (id) {
         id -> Integer,
         date -> Date,
@@ -7,3 +14,20 @@ table! {
         content -> Text,
     }
 }
+
+table! {
+    texts (text_id) {
+        text_id -> Integer,
+        page -> Integer,
+        description -> Text,
+        content -> Text,
+    }
+}
+
+joinable!(texts -> pages (page));
+
+allow_tables_to_appear_in_same_query!(
+    pages,
+    posts,
+    texts,
+);
